8063118691656cb597775bf51787b8f23fbe37cc
jiangce <5t4r3e2w1q>
2023-04-10 18:14:17 +0800

    feat(component):

--- packages/CW_Button/index.js
+++ packages/CW_Button/index.js
@@ -1,8 +1,5 @@
 import CW_Button from './src/CW_Button.vue';
 // import { App } from 'vue'
-import Vue from 'vue'
-import { Button } from 'element-ui'
-Vue.use(Button)
 CW_Button.install = ( Vue) => {
     Vue.component(CW_Button.name, CW_Button)
 }
